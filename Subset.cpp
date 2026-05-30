e0e470492173fc1a151a43cf1d6d9872695e38a8
Karan kumar soni <sonikaran926388@gmail.com>
2026-05-30 12:38:51 +0530

    Create Subset.cpp

---
+++ Subset.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void solve(vector<int>&nums, vector<int>& current, vector<vector<int>>&ans,int index){
+        if(index==nums.size()){
+            ans.push_back(current );
+        return;
+    }
+    
+    current.push_back(nums[index]);
+    solve (nums, current,ans,index+1);
+    current.pop_back();
+        solve (nums, current,ans,index+1);
+}
+    
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>current;
+        solve (nums, current,ans,0);
+        return ans;
+        
+    }
+};
