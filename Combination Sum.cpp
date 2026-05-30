2f280f90a0bed6f65c0999bca749448ed81123ad
Karan kumar soni <sonikaran926388@gmail.com>
2026-05-30 12:46:17 +0530

    Create Combination Sum.cpp

---
+++ Combination Sum.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void solve(int index, vector<int>&candidates ,int target ,vector<int>& current, vector<vector<int>>&ans){
+        if (target==0){
+            ans.push_back(current );
+            return;
+        }
+        if(target<0||index==candidates.size()){
+            return;
+        }
+        for(int i = index; i < candidates.size(); i++) {
+            if( candidates[i]>target)
+                continue;
+            current.push_back(candidates[i]);
+            solve (i,candidates, target -candidates[i], current,ans);
+            current.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>current;
+       solve(0,candidates, target, current,ans);
+        return ans;
+
+    }
+};
