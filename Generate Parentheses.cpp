19d63933cdf7191964624916bf86515652de00a7
Karan kumar soni <sonikaran926388@gmail.com>
2026-05-30 12:43:59 +0530

    Create Generate Parentheses.cpp

---
+++ Generate Parentheses.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void solve(int n ,int open,int close,string&current, vector<string>&ans){
+        if(open==n&&close==n){
+            ans.push_back(current);
+            return;
+        }
+        if (open<n){
+            current.push_back('(');
+            solve (
+                n,
+                open+1,
+                close,
+                current,
+                ans
+            );
+            current.pop_back();
+
+        }
+        if (close<open){
+            current.push_back(')');
+            solve (
+                n,
+                open,
+                close+1,
+                current,
+                ans
+            );
+            current.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+
+        string current = "";
+
+        solve (n,
+                  0,
+                  0,
+                  current,
+                  ans);
+
+        return ans;
+
+
+    }
+};
