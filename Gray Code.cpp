f428d77f6339f616f111bcc1da5b78177c9c28b9
Karan kumar soni <sonikaran926388@gmail.com>
2026-05-30 13:03:59 +0530

    Create Gray Code.cpp

---
+++ Gray Code.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<int>ans;
+        for(int i=0;i<(1<<n);i++){
+            ans.push_back(i^(i>>1));
+        }
+        return ans;
+    }
+};
