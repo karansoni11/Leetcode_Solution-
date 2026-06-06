f2faeb210a56ff8c1ec5a5233fab5bfa596ea661
Karan kumar soni <sonikaran926388@gmail.com>
2026-06-07 02:16:45 +0530

    Update Merge Two Sorted Lists.cpp

--- Merge Two Sorted Lists.cpp
+++ Merge Two Sorted Lists.cpp
@@ -1,5 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -10,6 +9,14 @@ using namespace std;
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
