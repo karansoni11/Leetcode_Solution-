1b20bf19e2a59429242a9ce7b73353d694012478
Karan kumar soni <sonikaran926388@gmail.com>
2026-06-07 02:14:47 +0530

    Update Link list Two Sum Number.cpp

--- Link list Two Sum Number.cpp
+++ Link list Two Sum Number.cpp
@@ -9,8 +9,15 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
